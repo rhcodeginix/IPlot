3e7ace3732f7a2a507767b6503bcacf2e467077e
rhcodeginix/Iplot.git <rhcodeginix/Iplot.git>
2025-04-25 14:13:59 +0530

    add some change

--- src/pages/login/loginForm.tsx
+++ src/pages/login/loginForm.tsx
@@ -17,7 +17,7 @@ import { useRouter } from "next/router";
 import Loader from "@/components/Loader";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import NameModal from "./nameModal";
-import VippsButton from "@/components/vipps";
+// import VippsButton from "@/components/vipps";
 
 const LoginForm: React.FC<{
   path?: any;
@@ -234,7 +234,7 @@ const LoginForm: React.FC<{
           Logg inn med Google
         </div>
 
-        <VippsButton />
+        {/* <VippsButton /> */}
       </div>
       {loading && (
         <div className="absolute top-0 left-0">
